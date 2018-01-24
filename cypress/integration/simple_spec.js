982667731306bc17b95dc87e73652f9109e5f8e8
ozaki <fs65ugumori@yahoo.co.jp>
2018-01-25 08:03:11 +0900

    サンプル作成中

--- cypress/integration/simple_spec.js
+++ cypress/integration/simple_spec.js
@@ -1,11 +1,6 @@
-describe('My First Test', function() {
-  it('Does not do much!', function() {
-    expect(true).to.equal(true)
-  })
-})
-
-describe('My First Test', function() {
-  it('Does not do much!', function() {
-    expect(true).to.equal(false)
-  })
-})
+describe("My First Test", function() {
+  it("Visits the Kitchen Sink", function() {
+    cy.visit("http://localhost:8080/");
+    cy.contains("type");
+  });
+});
